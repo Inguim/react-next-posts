b448b1b518ea2b94962b0f59f83fbce5a5913741
Inguim <igorazevedost@gmail.com>
2021-04-01 11:57:48 -0300

    Components: TextInput | Spec e Snapshot

---
+++ src/components/TextInput/TextInput.spec.jsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { TextInput } from '.';
+
+
+describe('<TextInput />', () => {
+    it('should have a value of searchValue', () => {
+        const fn = jest.fn();
+        render(<TextInput handleChange={fn} searchValue={'testando'} />);
+        
+        const input = screen.getByPlaceholderText(/type your search/i);
+        expect(input).toBeInTheDocument();
+
+        expect(input.value).toBe('testando');
+    });
+
+    it('should call handleChance function on each key pressed', () => {
+        const fn = jest.fn();
+        render(<TextInput handleChange={fn} />);
+
+        const input = screen.getByPlaceholderText(/type your search/i);
+        const value = 'o valor';
+
+        userEvent.type(input, value);
+
+        expect(input.value).toBe(value);
+        expect(fn).toBeCalledTimes(value.length);
+       
+    });
+
+    it('should match snapshot', () => {
+        const { container } = render(<TextInput />);
+        expect(container).toMatchSnapshot();
+    });
+});
