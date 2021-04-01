0877612b8524ec0ff02324cb8619a77289844e00
Inguim <igorazevedost@gmail.com>
2021-04-01 13:24:33 -0300

    Configuração do eslint, prettier, editorconfig e prop-types

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -3,42 +3,44 @@ import userEvent from '@testing-library/user-event';
 import { Button } from '.';
 
 describe('<Button />', () => {
-    it('should render the button with the text "Load more"', () => {
-        render(<Button text="Load More" />);
+  it('should render the button with the text "Load more"', () => {
+    const fn = jest.fn();
+    render(<Button text="Load more" disabled={false} onClick={fn} />);
 
-        expect.assertions(1);
+    expect.assertions(1);
 
-        const button = screen.getByRole('button', { name: /load more/i });
-        expect(button).toBeInTheDocument();
-    });
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeInTheDocument();
+  });
 
-    it('should call function on button click', () => {
-        const fn = jest.fn();
-        render(<Button text="Load More" onClick={fn} />);
+  it('should call function on button click', () => {
+    const fn = jest.fn();
+    render(<Button text="Load more" disabled={false} onClick={fn} />);
 
-        expect.assertions(1);
+    const button = screen.getByRole('button', { name: /load more/i });
 
-        const button = screen.getByRole('button', { name: /load more/i });
-        userEvent.click(button);
-        expect(fn).toHaveBeenCalledTimes(1);
-    });
+    userEvent.click(button);
 
-    it('should be disable when disable is true', () => {
-        render(<Button text="Load More" disabled={true} />);
-        const button = screen.getByRole('button', { name: /load more/i });
-        expect(button).toBeDisabled();
-    });
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
 
-    it('should be disable when disable is false', () => {
-        const fn = jest.fn();
-        render(<Button text="Load More" onClick={fn} disabled={false} />);
-        const button = screen.getByRole('button', { name: /load more/i });
-        expect(button).toBeEnabled();
-    });
+  it('should be disabled when disabled is true', () => {
+    const fn = jest.fn();
+    render(<Button text="Load more" disabled={true} onClick={fn} />);
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeDisabled();
+  });
 
-    it('should match snapshot', () => {
-        const fn = jest.fn();
-        const { container } = render(<Button text="Load More" onClick={fn} disabled={false} />);
-        expect(container.firstChild).toMatchSnapshot();
-    });
+  it('should be enabled when disabled is false', () => {
+    const fn = jest.fn();
+    render(<Button text="Load more" disabled={false} onClick={fn} />);
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeEnabled();
+  });
+
+  it('should match snapshot', () => {
+    const fn = jest.fn();
+    const { container } = render(<Button text="Load More" disabled={false} onClick={fn} />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
--- src/components/TextInput/TextInput.spec.jsx
+++ src/components/TextInput/TextInput.spec.jsx
@@ -1,35 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+
 import { TextInput } from '.';
 
-
 describe('<TextInput />', () => {
-    it('should have a value of searchValue', () => {
-        const fn = jest.fn();
-        render(<TextInput handleChange={fn} searchValue={'testando'} />);
-        
-        const input = screen.getByPlaceholderText(/type your search/i);
-        expect(input).toBeInTheDocument();
+  it('should have a value of searchValue', () => {
+    const fn = jest.fn();
+    render(<TextInput handleChange={fn} searchValue={'testando'} />);
 
-        expect(input.value).toBe('testando');
-    });
+    const input = screen.getByPlaceholderText(/type your search/i);
+    expect(input.value).toBe('testando');
+  });
 
-    it('should call handleChance function on each key pressed', () => {
-        const fn = jest.fn();
-        render(<TextInput handleChange={fn} />);
+  it('should call handleChange function on each key pressed', () => {
+    const fn = jest.fn();
+    render(<TextInput handleChange={fn} searchValue="um valor qualquer" />);
 
-        const input = screen.getByPlaceholderText(/type your search/i);
-        const value = 'o valor';
+    const input = screen.getByPlaceholderText(/type your search/i);
+    const value = 'o valor';
 
-        userEvent.type(input, value);
+    userEvent.type(input, value);
 
-        expect(input.value).toBe(value);
-        expect(fn).toBeCalledTimes(value.length);
-       
-    });
+    expect(input.value).toBe('um valor qualquer');
+    expect(fn).toHaveBeenCalledTimes(value.length);
+  });
 
-    it('should match snapshot', () => {
-        const { container } = render(<TextInput />);
-        expect(container).toMatchSnapshot();
-    });
+  it('should match snapshot', () => {
+    const fn = jest.fn();
+    const { container } = render(<TextInput handleChange={fn} searchValue="" />);
+    expect(container).toMatchSnapshot();
+  });
 });
